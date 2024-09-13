cce3e90baf25bfb338ce333a6943d8f826eecf9a
invi1998 <2267661826@qq.com>
2024-09-13 11:59:34 +0800

    胶囊体射线检测

--- Source/ClimbingSystem/Private/CustomComponents/CustomMovementComponent.cpp
+++ Source/ClimbingSystem/Private/CustomComponents/CustomMovementComponent.cpp
@@ -3,3 +3,28 @@
 
 #include "CustomComponents/CustomMovementComponent.h"
 
+#include "GameFramework/Character.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+TArray<FHitResult> UCustomMovementComponent::DoCapsuleTraceMultiByObject(const FVector& Start, const FVector& End, bool bShowDebug) const
+{
+	TArray<FHitResult> HitResults;
+
+	UKismetSystemLibrary::CapsuleTraceMultiForObjects(
+		this,
+		Start,
+		End,
+		ClimbCapsuleTraceRadius,
+		ClimbCapsuleTraceHalfHeight,
+		ClimbTraceObjectTypes,
+		false,
+		TArray<AActor*>(),
+		bShowDebug ? EDrawDebugTrace::ForDuration : EDrawDebugTrace::None,
+		HitResults,
+		false
+	);
+
+
+	return HitResults;
+
+}
--- Source/ClimbingSystem/Public/CustomComponents/CustomMovementComponent.h
+++ Source/ClimbingSystem/Public/CustomComponents/CustomMovementComponent.h
@@ -13,5 +13,18 @@ UCLASS()
 class CLIMBINGSYSTEM_API UCustomMovementComponent : public UCharacterMovementComponent
 {
 	GENERATED_BODY()
+
+private:
+	// 胶囊体射线检测
+	TArray<FHitResult> DoCapsuleTraceMultiByObject(const FVector& Start, const FVector& End, bool bShowDebug) const;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Character Movement: Climbing", meta=(AllowPrivateAccess = "true"))
+	float ClimbCapsuleTraceRadius = 50.0f;		// 胶囊体射线检测半径
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Character Movement: Climbing", meta=(AllowPrivateAccess = "true"))
+	float ClimbCapsuleTraceHalfHeight = 72.0f;	// 胶囊体射线检测高度
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Character Movement: Climbing", meta=(AllowPrivateAccess = "true"))
+	TArray<TEnumAsByte<EObjectTypeQuery>> ClimbTraceObjectTypes;	// 胶囊体射线检测对象类型
 	
 };
